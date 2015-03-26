987801d120b42aec058ae6f11b28d9e6f937e015
Leo Joseph <rleo2joseph@gmail.com>
2015-03-26 19:07:02 +0530

    Removed unwanted comments
    Removed unwanted comments

--- www/beacon.js
+++ www/beacon.js
@@ -7,11 +7,7 @@ exports.custom = function (){
                
                cordova.exec(
                             function callback(data) {
-                            // data comes from the NSDictionary instance (jsonObj) from our Objective C code.
-                            // Take a look at the cordovaGetFileContents method from FileWriter.m and you'll see
-                            // where we add dateStr as a property to that Dictionary object.
-                            var msg = 'Current file contents: <br />' + data.dateStr;
-                            contentsDiv.innerHTML = msg;
+
                             },
                             function errorHandler(err) {
                             alert('Error');
